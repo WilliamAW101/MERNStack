29c07c07f979d036cd5c8792f578b885ca97b198
Alan-Alberto12 <alanjoalberto@gmail.com>
2025-10-09 18:29:40 -0400

    Phase 4 Finished

--- cards/frontend/src/components/Login.tsx
+++ cards/frontend/src/components/Login.tsx
@@ -1,18 +1,35 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 function Login()
 {
+    function handleSetLoginName( e: any ) : void
+{
+setLoginName( e.target.value );
+}
+function handleSetPassword( e: any ) : void
+{
+setPassword( e.target.value );
+}
+const navigate = useNavigate();
+const [message,setMessage] = useState('');
+const [loginName,setLoginName] = useState('');
+const [loginPassword,setPassword] = useState('');
 function doLogin(event:any) : void
 {
-event.preventDefault();
-alert('doIt()');
-}
+    event.preventDefault();
+    alert('doIt() ' + loginName + ' ' + loginPassword);
+    navigate('/cards');
+};
 return(
 <div id="loginDiv">
 <span id="inner-title">PLEASE LOG IN</span><br />
-<input type="text" id="loginName" placeholder="Username" /><br />
-<input type="password" id="loginPassword" placeholder="Password" /><br />
+Login: <input type="text" id="loginName" placeholder="Username"
+onChange={handleSetLoginName} />
+Password: <input type="password" id="loginPassword" placeholder="Password"
+onChange={handleSetPassword} />
 <input type="submit" id="loginButton" className="buttons" value = "Do It"
 onClick={doLogin} />
-<span id="loginResult"></span>
+<span id="loginResult">{message}</span>
 </div>
 );
 };
