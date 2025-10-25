670bab9dc2aee7b00259bff91440e8c9d6bba4a5
Angelo <angelowhite871@gmail.com>
2025-10-25 11:36:14 -0400

    created endpoints for password reset functionality

--- cards/backend/routes/authRoutes.js
+++ cards/backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const validator = require('validator');
 
 // import functions
-const connectToDatabase = require('../config/database.js')
+const connectToDatabase = require('../config/database.js');
 
 const {
     hashPass,
@@ -11,12 +11,18 @@ const {
     generateToken,
     genEmailToken,
     sendVerificationEmail,
-    checkExpired
+    checkExpired,
+    genResetCode,
+    sendPasswordChangeToken
 } = require('../utils/authentication.js');
 
 const {
     responseJSON
 } = require('../utils/json.js')
+
+const {
+    validateEmail
+} = require('../utils/validation.js')
 
 // Simple ping route, good to make sure server is running
 router.get('/ping', async (req, res) => {
@@ -34,7 +40,10 @@ router.post('/login', async (req, res) => {
     // Payload receiving: userName, password
     // Payload sending: id, firstName, lastName, token, error
     try {
-        const { userName, password } = req.body;
+        const { 
+            userName,
+            password 
+        } = req.body;
 
         // Validate input
         if (!userName || !password) {
@@ -159,6 +168,97 @@ router.post('/signup', async (req, res) => {
         responseJSON(res, false, { code: 'Internal server error' }, 'Failed to communicate with endpoint', 500);
     }
 
+});
+
+router.get('/sendCode', async (req, res) => {
+    try {
+        const db = await connectToDatabase();
+        const codeCollection = db.collection('passwordVerify');
+        
+        const email = req.query.email;
+        const validatedEmail = validateEmail(res, email);
+        if (validatedEmail == null)
+            return; // already set response within function
+        // // Email verification
+        const returnedResult = await genResetCode(res, validatedEmail);
+        if (returnedResult == null)
+            return;
+
+        const id = returnedResult.id; // this will store the userID 
+        const code = returnedResult.code;
+        const storeCode = {
+            code: code,
+            id: id,
+            createdAt: { type: Date, default: Date.now, expires: 3600 }
+        };
+        const success = await sendPasswordChangeToken(validatedEmail, code);
+        if (!success) {
+            return responseJSON(res, false, { code: 'Email address does not exist' }, 'Failed to send password change code', 553);
+        }
+        const result = await codeCollection.insertOne(storeCode);  
+
+        const ret = {
+          id: result.insertedId,
+        };
+        responseJSON(res, true, ret, 'Code emailed successfully!', 201);
+    } catch (e) {
+        console.error('Signup error:', e);
+        responseJSON(res, false, { code: 'Internal server error' }, 'Failed to communicate with endpoint', 500);
+    }
+});
+
+// payload will need a verification code that will be sent via email that the user has verified
+router.post('/changePassword', async (req, res) => {
+    const requiredFields = {
+        code,
+        newPassword,
+        samePassword
+    } = req.body;
+
+    // check to see if everything is filled out
+    const missingFields = Object.entries(requiredFields)
+        .filter(([, value]) => value === undefined || value === null || value === '')
+        .map(([key]) => key);
+    if (missingFields.length > 0) {
+        return responseJSON(res, false, { code: 'Bad Request' }, `Missing required field${missingFields.length > 1 ? 's' : ''}: ${missingFields.join(', ')}`, 400);
+    }
+
+    if (requiredFields.newPassword != requiredFields.samePassword)
+        return responseJSON(res, false, { code: 'Unauthorized' }, 'Passwords do not match', 401)
+
+    const db = await connectToDatabase();
+    const codeCollection = db.collection('passwordVerify');
+    // match the code
+    const verification = await codeCollection.findOne({ code: requiredFields.code });
+    if (!verification) {
+      return responseJSON(res, false, { code: 'Unauthorized' }, 'Invalid Code', 401); 
+    }
+
+    // change the password
+    const userCollection = db.collection('user');
+    const hashedPassword = await hashPass(requiredFields.newPassword);
+
+
+    // replace the old password with new
+    const result = await userCollection.updateOne(
+        {_id: verification.id},
+        { $set: {password: hashedPassword} }
+    );
+
+    // delete the password verify document from collection
+    const deleteResult = await codeCollection.deleteOne(
+        { code: requiredFields.code }
+    );
+    if (result.deletedCount === 0) {
+      console.log('No document found with that ID.');
+    } else {
+      console.log('Document deleted successfully.');
+    }
+
+    const ret = {
+      id: result.insertedId,
+    };
+    responseJSON(res, true, ret, 'Password Change Success!', 201);
 });
 
 // Front end does not need to know this crap exists, just that it works
--- cards/backend/utils/authentication.js
+++ cards/backend/utils/authentication.js
@@ -1,35 +1,41 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const sgMail = require('@sendgrid/mail');
+const connectToDatabase = require('../config/database');
+const crypto = require('crypto');
+
+const {
+    responseJSON
+} = require('../utils/json.js')
 
 const hashPass = async (password) => { // its in the name
-    const salt = 10;
-    return await bcrypt.hash(password, salt);
+  const salt = 10;
+  return await bcrypt.hash(password, salt);
 }
 
 const verifyPass = async (password, hashedPassword) => { // just unhashing the password and comparing
-    return await bcrypt.compare(password, hashedPassword);
+  return await bcrypt.compare(password, hashedPassword);
 }
 
 const generateToken = (user) => { // creates token, I don't think I need to add anything else to the payload?
-    return jwt.sign(
-        { id: user._id, userName: user.userName },
-        process.env.JWT_SECRET,
-        { expiresIn: process.env.JWT_EXPIRES_IN || '1h' }
-    );
+  return jwt.sign(
+      { id: user._id, userName: user.userName },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN || '1h' }
+  );
 }
 
 const checkExpired = (token) => { // validating the token
-    try {
-        return jwt.verify(token, process.env.JWT_SECRET);
-    } catch (e) {
-        return null;
-    }
+  try {
+      return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (e) {
+      return null;
+  }
 }
 
 const refreshToken = (token) => { // refreshing token expiration
-    const decode = jwt.decode(token);
-    return generateToken(decode);
+  const decode = jwt.decode(token);
+  return generateToken(decode);
 }
 
 const sendVerificationEmail = async (to, token) => {
@@ -61,12 +67,54 @@ const sendVerificationEmail = async (to, token) => {
   }
 }
 
-const genEmailToken = (email) => {
-    return jwt.sign(
-        { email },
-        process.env.JWT_SECRET,
-        { expiresIn: '1d' }
-    );
+const sendPasswordChangeToken = async (to, code) => {
+  sgMail.setApiKey(process.env.SENDGRIND_API_KEY);
+
+  const mailOptions = {
+    from: `"CraigTag" <${process.env.EMAIL_USER}>`,
+    to,
+    subject: 'Code for Password Change',
+    html: `
+      <h2>Your Verification Code is:</h2>
+      <h3>${code}</h3>
+    `
+  };
+
+  try {
+    await sgMail.send(mailOptions);
+    console.log(`Verification email sent to ${to}`);
+    return true;
+  } catch (err) {
+    console.error('Error sending email:', err);
+    return false;
+  }
 }
 
-module.exports = { hashPass, verifyPass, generateToken, checkExpired, sendVerificationEmail, genEmailToken };
+const genEmailToken = (email) => {
+  return jwt.sign(
+    { email },
+    process.env.JWT_SECRET,
+    { expiresIn: '1d' }
+  );
+}
+
+const genResetCode = async (res, email) => {
+
+  const db = await connectToDatabase();
+  const userCollection = db.collection('user');
+
+  // Find user by email
+  const user = await userCollection.findOne({ email });
+  if (!user) {
+    responseJSON(res, false, { code: 'Unauthorized' }, 'Email does not exist', 401)
+    return null;
+  }
+
+  // generating code for user to provide
+  const length = 16;
+  const code = crypto.randomBytes(length).toString('hex');
+  const id = user._id;
+  return { id, code };
+}
+
+module.exports = { hashPass, verifyPass, generateToken, checkExpired, sendVerificationEmail, genEmailToken, sendPasswordChangeToken, genResetCode };
