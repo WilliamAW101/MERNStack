7e6996013cbad6581598417a95c9486f84e6772d
Alan-Alberto12 <alanjoalberto@gmail.com>
2025-11-09 18:59:43 -0500

    Get Post + Delete Post + Update Post

--- cards/backend/routes/uploads.js
+++ cards/backend/routes/uploads.js
@@ -2,10 +2,16 @@ const express = require('express');
 const crypto = require('crypto');
 const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
-const { authenticateToken } = require('../middleware/authMiddleware'); 
+const { authenticateToken } = require('../middleware/authMiddleware');
 
 const router = express.Router();
-const s3 = new S3Client({ region: process.env.AWS_REGION });
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+});
 
 /**
  * Client calls this to get a 60s presigned PUT URL.
