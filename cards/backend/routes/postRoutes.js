a8a4d602ac516a985288e2dc0bdc50dd9fc17435
Angelo <angelowhite871@gmail.com>
2025-11-17 00:36:25 -0500

    bugfix toString()

--- cards/backend/routes/postRoutes.js
+++ cards/backend/routes/postRoutes.js
@@ -133,7 +133,7 @@ router.put('/updatePost', authenticateToken, async (req, res) => {
             return responseJSON(res, false, { code: 'Not Found' }, 'Post not found', 404);
         }
 
-        if (post.userId !== userId.toString()) {
+        if (post.userId !== userId) {
             return responseJSON(res, false, { code: 'Forbidden' }, 'You do not have permission to update this post', 403);
         }
 
