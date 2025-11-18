a0e006c78613851e28e629a3410a24a0db32bddb
thainguyen1405 <quocthai14052004@gmail.com>
2025-11-17 21:43:09 -0500

    Clean up: Remove all files except mobile_app for better performance

--- cards/backend/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/tests'],
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
-  collectCoverageFrom: ['routes/**/*.js', 'utils/**/*.js', 'config/**/*.js'],
-  moduleFileExtensions: ['js', 'json'],
-  verbose: true,
-};
